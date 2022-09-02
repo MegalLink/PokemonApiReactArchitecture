52c9d127425daa66c42fe96d0625e6e2883bc963
unknown <jeferson.narvaez.dev@gmail.com>
2022-09-02 12:43:35 -0500

    Add format linte , coverage configuration and husky precommit

--- src/components/CreatePokemon/CreatePokemons.tsx
+++ src/components/CreatePokemon/CreatePokemons.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React from 'react'
+import { Typography } from '@mui/material'
 export const CreatePokemon = () => {
-  return <Typography color="primary">Create Pokemon</Typography>;
-};
+  return <Typography color='primary'>Create Pokemon</Typography>
+}
